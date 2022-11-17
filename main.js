dd9829b59b6e62d4681b1d0f1886aa39a4dd7217
Araceli Gittardi <araceligittardi@tdf.edu.ar>
2022-11-16 22:03:03 -0300

    ejercicio 1,2,3

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let imput1 = document.getElementById("texto");
+let imput2 = document.getElementById("url");
+let imput3 = document.getElementById("parrafo");
+let btn = document.getElementById("btn");
 
+btn.addEventListener("click", () => {
+
+    let textoValue = texto.value;
+    let urlValue = url.value;
+    let parrafoValue = parrafo.value;
+
+    document.getElementById("card-title").innerHTML = texto.value;
+    document.getElementById("card-img-top").src = url.value;
+
+
+});
